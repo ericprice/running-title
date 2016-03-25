87bb051a62d786b8f099e0aebac01d7f7172b00c
Eric Price <eric@ericprice.cc>
2016-03-25 14:22:01 -0400

    Add some options

--- runningTitle.jsx
+++ runningTitle.jsx
@@ -1,37 +1,51 @@
-var currentDoc = app.activeDocument;
-var pages = currentDoc.pages;
-var filePath = currentDoc.filePath.fsName;
-var titleText = prompt('Type your title without spaces and punctuation:', '', 'Type your title without spaces and punctuation:').toLowerCase().split('');
+var doc = app.activeDocument,
+    pages = doc.pages,
+    promptText = 'Type your title without spaces and punctuation:',
+    titleText = prompt(promptText, '', promptText).toLowerCase().split(''),
+    programName = 'curatorialstudies',
+    letterDirPrefix = 'letters',
+    letterFileFormat = 'ai';
 
 function isEven(n) {
   return n % 2 === 0;
 }
 
-var through = 0;
-var j = 0;
+function main(color) {
+  var letters = doc.layers.add({name: 'letters-' + color}).move(LocationOptions.AT_END),
+      j = 0;
 
-for (var i = 0; i < pages.length; i++) {
-  var letter = pages[i].place(new File(filePath + '/letters/letter-' + titleText[j] + '.ai'));
-  if (isEven(i) && i !== 0) {
-    letter.geometricBounds = [0, 8.5 + ' in', 11 + ' in', 17 + ' in'];
-  } else {
-    letter.geometricBounds = [0, 0, 11 + ' in', 8.5 + ' in'];
+  // Gray text, in this case, is used for the program name
+  if (color === 'gray') {
+    titleText = programName.split('');
   }
 
-  if (i == (pages.length - 1)) {
-    for (j; j < titleText.length; j++) {
-      var letter = pages[i].place(new File(filePath + '/letters/letter-' + titleText[j] + '.ai'));
-      if (isEven(i) && i !== 0) {
-        letter.geometricBounds = [0, 8.5 + ' in', 11 + ' in', 17 + ' in'];
-      } else {
-        letter.geometricBounds = [0, 0, 11 + ' in', 8.5 + ' in'];
-      }
+  function addLetters(i, j) {
+    var letter = pages[i].place(new File(doc.filePath.fsName + '/' + letterDirPrefix + '-' + color + '/' + titleText[j] + '.' + letterFileFormat));
+    if (isEven(i) && i !== 0) {
+      letter.geometricBounds = [0, '50%', '100%', '100%'];
+    } else {
+      letter.geometricBounds = [0, 0, '100%', '50%'];
     }
   }
 
-  if (j == (titleText.length - 1)) {
-    j = 0;
-  } else {
-    j++;
+  for (var i = 0; i < pages.length; i++) {
+    addLetters(i, j);
+
+    // Car crash on last page
+    if (i == (pages.length - 1)) {
+      addLetters(i, j);
+    }
+
+    // Loop through the text multiple times, if necessary, to fill all pages
+    if (j == (titleText.length - 1)) {
+      j = 0;
+    } else {
+      j++;
+    }
   }
+
+  letters.locked = true;
 }
+
+main('yellow');
+main('gray');
